d0bc13a72c394b35196724b06852f01420eefcba
ClarkLe01 <clark.le.goldenowl@gmail.com>
2023-02-28 12:07:59 +0700

    SC-13 add celery config and fix error

--- utils/models.py
+++ utils/models.py
@@ -1,17 +1,17 @@
-from django.db import models
-from user.models import User
-from datetime import datetime, timedelta
+# from django.db import models
+# from user.models import User
+# from datetime import datetime, timedelta
 
 
 # Create your models here.
-class TOTP(models.Model):
-    lifeTime = models.IntegerField(default=60)
-    code = models.CharField(max_length=6)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.code
-
-    def isExpired(self):
-        return datetime.now() >= timedelta(seconds=self.lifeTime) + self.created
+# class TOTP(models.Model):
+#     lifeTime = models.IntegerField(default=60)
+#     code = models.CharField(max_length=6)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     created = models.DateTimeField(auto_now_add=True)
+#
+#     def __str__(self):
+#         return self.code
+#
+#     def isExpired(self):
+#         return datetime.now() >= timedelta(seconds=self.lifeTime) + self.created
