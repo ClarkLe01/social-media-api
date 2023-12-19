b5ca7f88e40cc1d25480fbe807634ae3556e6ef1
Clark Le <lenguyenhunganh2001@gmail.com>
2023-12-19 16:18:36 +0700

    Update doc swagger

--- core/urls.py
+++ core/urls.py
@@ -25,19 +25,6 @@ from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Social Web API",
-        default_version="v1",
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="clark.le.goldenowl@gmail.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
-
-schema_view = get_schema_view(
-    openapi.Info(
         title="Social Media Web API",
         default_version="v1",
         description="Test description",
