cbf6d0ff470f83ee25cf6857146ea8f855857af5
Clark Le <lenguyenhunganh2001@gmail.com>
2023-12-28 19:51:58 +0700

    Fix

--- core/celery.py
+++ core/celery.py
@@ -7,8 +7,6 @@ from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-cronitor.api_key = "a338a97cfdef421db4e4c1ac03312f7c"
-cronitor.environment = os.getenv("ENVIRONMENT")
 app = Celery("core")
 if os.getenv("ENVIRONMENT") == "staging":
     app = Celery(
@@ -16,7 +14,10 @@ if os.getenv("ENVIRONMENT") == "staging":
         broker_use_ssl={"ssl_cert_reqs": ssl.CERT_NONE},
         redis_backend_use_ssl={"ssl_cert_reqs": ssl.CERT_NONE},
     )
-cronitor.celery.initialize(app)
+if os.getenv("CRONITOR_API_KEY"):
+    cronitor.api_key = os.getenv("CRONITOR_API_KEY")
+    cronitor.environment = os.getenv("ENVIRONMENT")
+    cronitor.celery.initialize(app)
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
