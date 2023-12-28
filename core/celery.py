3205023f9581fdb3b6ed398fc56a4bbc839afd41
Clark Le <lenguyenhunganh2001@gmail.com>
2023-12-28 18:06:21 +0700

    Update

--- core/celery.py
+++ core/celery.py
@@ -1,12 +1,14 @@
 import os
 import ssl
 
+import cronitor.celery
 from celery import Celery
 from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-
+cronitor.api_key = "a338a97cfdef421db4e4c1ac03312f7c"
+cronitor.environment = os.getenv("ENVIRONMENT")
 app = Celery("core")
 if os.getenv("ENVIRONMENT") == "staging":
     app = Celery(
@@ -14,6 +16,7 @@ if os.getenv("ENVIRONMENT") == "staging":
         broker_use_ssl={"ssl_cert_reqs": ssl.CERT_NONE},
         redis_backend_use_ssl={"ssl_cert_reqs": ssl.CERT_NONE},
     )
+cronitor.celery.initialize(app)
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
