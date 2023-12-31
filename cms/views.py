93db8a74985755247bd7429f3bf7e2f565f6f42a
Clark Le <lenguyenhunganh2001@gmail.com>
2023-12-31 11:30:44 +0700

    Add cms api

--- cms/views.py
+++ cms/views.py
@@ -1,21 +1,46 @@
 from django.shortcuts import render
-from rest_framework import status
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from cms.permissions import IsSuperAdminUser
 from cms.serializers import AdminTokenObtainPairSerializer
 from post.models import Post
+from post.serializers import CreatePostSerializer
 from user.models import User
+from user.serializers import UserProfileSerializer
 
 
 # Create your views here.
-class CmsPostListApi:
-    pass
+class CmsPostListApi(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = CreatePostSerializer
+    permission_classes = [IsSuperAdminUser]
 
 
-class CmsUserListApi:
-    pass
+class CmsUserListApi(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsSuperAdminUser]
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class CmsLoginApi(TokenObtainPairView):
